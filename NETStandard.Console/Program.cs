1c0e4b1e2f9f47075b638a83d65c3719d62b1c4c
AMAIISDOM\bgarcia <BryanAnthony.Garcia@navitaire.com>
2017-10-20 22:48:49 +0800

    .NET framework

---
+++ NETStandard.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using NETStandard.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETStandard.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonService ps = new PersonService();
+            var personList = ps.GetPersons();
+
+            foreach (var person in personList)
+            {
+                System.Console.WriteLine($"{person.Name}, {person.Age} years old, from {person.City}");
+            }
+
+            System.Console.ReadLine();
+        }
+    }
+}
