8e52ed0dfe98601736a19037924a8aeaf472261e
AMAIISDOM\bgarcia <BryanAnthony.Garcia@navitaire.com>
2017-10-20 23:15:53 +0800

    Xamarin.Forms

---
+++ NETStandard.Core/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using NETStandard.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NetStandard.Core
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            PersonService ps = new PersonService();
+            PersonListView.ItemsSource = ps.GetPersons();
+        }
+    }
+}
